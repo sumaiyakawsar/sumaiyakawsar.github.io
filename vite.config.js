28223a379a703ccb855b4a4ccd1a193e84618d2e
Sumaiya Kawsar <sumaiya.kawsar693@gmail.com>
2025-01-13 16:52:09 +0300

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
  
 export default defineConfig({
   plugins: [react()],
+  base: '/sumaiyakawsar.github.io/',
   css: {
     modules:{
       localConvention: "camelCase",
